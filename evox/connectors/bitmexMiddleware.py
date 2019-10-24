7ade977877dd113d8687dcc6a93f717c170e5f0d
Lucas P Torres de Araújo <lucast0rr3s@gmail.com>
2019-10-24 20:11:57 -0300

    Added methods BitmexMiddleware
    fetchOpenBuyOrders, fetchOpenSellOrders, fetchBalance, fetchTicker added.

--- evox/connectors/bitmexMiddleware.py
+++ evox/connectors/bitmexMiddleware.py
@@ -10,8 +10,10 @@ import bitmex
 
 import json
 
+
 class BitmexException(Exception):
     pass
+
 
 class BitmexMiddleware(object):
     '''
@@ -21,7 +23,7 @@ class BitmexMiddleware(object):
         ------------
         key : str
             The public key from Bitmex API account
-        
+
         secret : str
             The secret key from Bitmex API account
     '''
@@ -39,7 +41,7 @@ class BitmexMiddleware(object):
     @property
     def client(self):
         return self._client
-    
+
     def createLimitOrder(self, symbol, side, quantity, price):
         '''
             Post a new order for your account.
@@ -70,7 +72,7 @@ class BitmexMiddleware(object):
         except Exception as error:
             print(f'Error creating limit order ({side}).')
             raise BitmexException(error)
-    
+
     def createMarketOrder(self, symbol, side, quantity):
         '''
             Post a new order of type market for your account.
@@ -95,7 +97,62 @@ class BitmexMiddleware(object):
         except AttributeError:
             raise BitmexException('Error setting order side (buy or sell).')
         except Exception as error:
-            print(f'Error creating limit order ({side}).')
+            print(f'Error creating market order ({side}).')
+            raise BitmexException(error)
+
+    def fetchTicker(self):
+        try:
+            return self.client.Quote.Quote_get(symbol='XBT')
+        except:
+            pass
+
+    def fetchBalance(self, currency='XBt'):
+        '''
+            Get current asset balance if have passed parameter or,
+            if not, return each assets balances. XBt by default.
+
+            :param asset: -
+            :type asset: str
+
+            :returns: dictionary with API response
+
+            API Response Example
+            --------
+            {
+                "account": 0,
+                "currency": "string",
+                "prevDeposited": 0,
+                "prevWithdrawn": 0,
+                "prevTransferIn": 0,
+                "prevTransferOut": 0,
+                "prevAmount": 0,
+                "prevTimestamp": "2019-10-24T12:17:45.777Z",
+                "deltaDeposited": 0,
+                "deltaWithdrawn": 0,
+                "deltaTransferIn": 0,
+                "deltaTransferOut": 0,
+                "deltaAmount": 0,
+                "deposited": 0,
+                "withdrawn": 0,
+                "transferIn": 0,
+                "transferOut": 0,
+                "amount": 0,
+                "pendingCredit": 0,
+                "pendingDebit": 0,
+                "confirmedDebit": 0,
+                "timestamp": "2019-10-24T12:17:45.777Z",
+                "addr": "string",
+                "script": "string",
+                "withdrawalLock": [
+                    "string"
+                ]
+            }
+        '''
+        try:
+            balance = self.client.User.User_getWallet(currency=currency)
+            return balance.result()[0]
+        except Exception as error:
+            print(f'Error fetching asset balance.')
             raise BitmexException(error)
 
     def fetchOrders(self, *args):
@@ -116,7 +173,7 @@ class BitmexMiddleware(object):
                 orders = self.client.Order.Order_getOrders(symbol=symbol)
             else:
                 orders = self.client.Order.Order_getOrders()
-            
+
             return orders.result()[0]
 
         except Exception as error:
@@ -150,6 +207,59 @@ class BitmexMiddleware(object):
             print(f'Error fetching all orders.')
             raise BitmexException(error)
 
+    def fetchOpenBuyOrders(self, *args):
+        '''
+            Fetch all buy orders on a symbol,
+            If the symbol is not sent, buy open orders for all
+            symbols will be returned in an array.
+
+            :param symbol: -
+            :type symbol: str
+
+            :returns: list of dictionaries with API response
+        '''
+        try:
+            if args:
+                symbol = str(args[0])
+                buys = self.fetchOpenOrders(symbol)
+            else:
+                buys = self.fetchOpenOrders()
+
+            buys = [buy for buy in buys
+                    if buy['side'].lower() == self.SIDE_BUY.lower()]
+            return buys
+
+        except Exception as error:
+            print('Error fetching open buy orders.')
+            raise BitmexException(error)
+
+    def fetchOpenSellOrders(self, *args):
+        '''
+            Fetch all sell orders on a symbol,
+            If the symbol is not sent, sell open orders for all
+            symbols will be returned in an array.
+
+            :param symbol: -
+            :type symbol: str
+
+            :returns: list of dictionaries with API response
+        '''
+        try:
+            if args:
+                symbol = str(args[0])
+                sells = self.fetchOpenOrders(symbol)
+            else:
+                sells = self.fetchOpenOrders()
+
+            sells = [sell for sell in sells
+                     if sell['side'].lower() == self.SIDE_SELL.lower()]
+            return sells
+
+        except Exception as error:
+            print('Error fetching open sell orders.')
+            raise Exception(error)
+
+
 if __name__ == '__main__':
     key = 'G05-RxwEt6Wl8n2khC-nvuJf'
     secret = '7yw2poyvsEoDjjoGfsQd-JQlGpVE7wwv2vg9swzmpGMRH36t'
@@ -161,7 +271,7 @@ if __name__ == '__main__':
     #                                    quantity=1)
     # print(type(order))
     # print(order)
-    orders = my_client.fetchOpenOrders()
-    print(type(orders))
-    print(len(orders))
-    print(orders)
+    balance = my_client.fetchTicker()
+    print(type(balance))
+    # print(len(balance))
+    print(balance)
